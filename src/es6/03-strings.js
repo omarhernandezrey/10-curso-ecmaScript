92a67b84518def6f5f51c1a687068605e75fabec
omarhernandezrey <hernandezreyomar@gmail.com>
2023-08-28 12:48:56 -0500

    05 ES6: strings

---
+++ src/es6/03-strings.js
@@ -0,0 +1,24 @@
+
+// Declaración de variables
+let hello = 'Hello';
+let world = 'World';
+
+// Concatenación de cadenas usando el operador '+'
+let epicPhrase = hello + ' ' + world + '!';
+// console.log(epicPhrase);
+
+// Template literales para concatenar cadenas
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// Cadena de múltiples líneas usando '\n' para saltos de línea
+let lorem = 'esto es un string \n ' + 'esto es otra linea';
+
+// Cadena de múltiples líneas usando template literales (backticks)
+let lorem2 = `Esta es una frase épica
+la continuación de esa frase épica.`;
+
+// Imprimir las cadenas en la consola
+console.log(lorem);
+console.log(lorem2);
+
