074f83dcfadcae500becc46aeb40d239c2acc2fa
omarhernandezrey <hernandezreyomar@gmail.com>
2023-08-29 11:25:15 -0500

    07 ES6: asignación de desestructuración

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,19 @@
+// Arrays destructuring
+let fruits = ['Apple', 'Banana'];
+
+// Desestructuración de un array en variables 'a' y 'b'
+let [a, b] = fruits;
+console.log(a, fruits[1]); // Imprime 'Apple Banana'
+
+// Object destructuring
+let user = {
+    username: 'Oscar',
+    age: 34
+};
+
+// Desestructuración de un objeto en variables 'username' y 'age'
+let {username, age} = user;
+console.log(username, user.age); // Imprime 'Oscar 34'
+
+
+
