2f8faf45aac9b3c41e7208a1dbdc35b9db95a048
omarhernandezrey <hernandezreyomar@gmail.com>
2023-08-30 09:50:39 -0500

    13 ES6: module

---
+++ src/es6/09-module.js
@@ -0,0 +1,7 @@
+// Importa la función 'hello' desde el archivo "./module.js"
+// El archivo "./module.js" contiene la implementación de la función 'hello'
+import hello from "./module.js";
+
+// Llama a la función 'hello' que fue importada desde el módulo
+// La función 'hello' imprimirá "hello!" en la consola
+hello();
---
+++ src/es6/module.js
@@ -0,0 +1,7 @@
+// Declaración de una función flecha llamada 'hello'
+const hello = () => {
+    console.log('hello!');
+}
+
+// Exportación de la función 'hello' como valor predeterminado
+export default hello;
