8e862aa7e0a06f4b8933ae670c3f80d256bfc02b
omarhernandezrey <hernandezreyomar@gmail.com>
2023-08-30 14:20:52 -0500

    18 ES6: set-add

---
+++ src/es6/11-set-add.js
@@ -0,0 +1,9 @@
+// Creación de un nuevo Set llamado 'list'
+const list = new Set();
+
+// Agregar elementos al Set utilizando el método 'add'
+list.add('item 1'); // Agrega 'item 1' al Set
+list.add('item 2').add('item 3'); // Agrega 'item 2' y 'item 3' al Set en una cadena
+
+// Imprimir el contenido del Set 'list'
+console.log(list);
