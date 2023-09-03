e9857f73d50d374350725d0c22440fae1e14d7bf
omarhernandezrey <hernandezreyomar@gmail.com>
2023-09-03 14:00:34 -0500

    19 ES8: object entries y object values

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,19 @@
+// Declaración de un objeto llamado 'countries' con pares clave-valor
+const countries = {
+    MX: 'Mexico',
+    CO: 'Colombia',
+    CL: 'Chile',
+    PE: 'Peru'
+};
+
+// Utilización de 'Object.entries()' para convertir el objeto en una matriz de pares clave-valor
+console.log(Object.entries(countries));
+
+// Resultado:
+// [
+//     ['MX', 'Mexico'],
+//     ['CO', 'Colombia'],
+//     ['CL', 'Chile'],
+//     ['PE', 'Peru']
+// ]
+
---
+++ src/es8/01-object-values.js
@@ -0,0 +1,13 @@
+// Declaración de un objeto llamado 'countries' con pares clave-valor
+const countries = {
+    MX: 'Mexico',
+    CO: 'Colombia',
+    CL: 'Chile',
+    PE: 'Peru'
+};
+
+// Utilización de 'Object.values()' para obtener un array de los valores del objeto
+console.log(Object.values(countries));
+
+// Resultado:
+// ['Mexico', 'Colombia', 'Chile', 'Peru']
