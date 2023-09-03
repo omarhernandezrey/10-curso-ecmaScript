418f18e618f24ff767250185a1ad0798ee843ab8
omarhernandezrey <hernandezreyomar@gmail.com>
2023-09-03 11:18:03 -0500

    19 ES7: exponentiation operator y array includes

---
+++ src/es7/00-exponential.js
@@ -0,0 +1,5 @@
+// Cálculo de la potencia de 3 elevado a la cuarta potencia (3^4)
+const data = 3 ** 4;
+
+// Imprime el resultado en la consola
+console.log(data); // 81
---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,24 @@
+// Declaración de un array llamado 'numbers'
+let numbers = [1, 3, 4, 6, 7, 8];
+
+// Verificar si el número 4 está presente en el array 'numbers'
+console.log(numbers.includes(4)); // true, porque 4 está en el array
+
+// Verificar si el número 9 está presente en el array 'numbers'
+console.log(numbers.includes(9)); // false, porque 9 no está en el array
+
+// Declaración de un array llamado 'list'
+const list = ['oscar', 'David', 'Ana'];
+
+// Verificar si 'oscar' está presente en el array 'list'
+console.log(list.includes('oscar')); // true, porque 'oscar' está en el array
+
+// Verificar si 'Oscar' está presente en el array 'list'
+console.log(list.includes('Oscar')); // false, porque 'Oscar' no está en el array
+
+
+
+
+
+
+// expected output: true
