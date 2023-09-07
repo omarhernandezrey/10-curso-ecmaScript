da42e82247bade470d029eac2978d9086afe912a
omarhernandezrey <hernandezreyomar@gmail.com>
2023-09-07 15:58:32 -0500

    26 ES11: optional chaining

---
+++ src/es11/00-optional-chainiong.js
@@ -0,0 +1,10 @@
+const users = {
+ gndx: {
+    country:'mx'
+ },
+ ana: {
+    country:"co"
+ }
+};
+
+console.log(users?.bebeloper?.country);//Despues de poner el signo de pregunda ya no sale el erorror si no el undefined
