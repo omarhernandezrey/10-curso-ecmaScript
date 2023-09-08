57ee46b1795d09a753fc122ff7f268136fe97a96
omarhernandezrey <hernandezreyomar@gmail.com>
2023-09-08 15:40:52 -0500

    28 ES11: Promise.allSettled

--- src/es11/03-promise-allsettled.js
+++ src/es11/03-promise-allsettled.js
@@ -1,3 +1,6 @@
-const promise1 = new Promise(() => PromiseRejectionEvent("Reject"));
+const promise1 = new Promise((resolve, reject) => reject("Reject"));
 const promise2 = new Promise((resolve, reject) => resolve("resolve"));
-const promise3 = new Promise((resolve, reject)=> resolve(Resolve2))
+const promise3 = new Promise((resolve, reject)=> resolve("Resolve 2"));
+
+Promise.allSettled([promise1, promise2, promise3])
+ .then(response => console.log(response));
