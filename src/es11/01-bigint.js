c62fcaf6a609d5232ff0146ee8f77021b3e73bc5
omarhernandezrey <hernandezreyomar@gmail.com>
2023-09-08 10:38:55 -0500

    27 ES11: BigInt y Nullish

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const aBigNumber = 8515154151515151515151541445n;
+const anotherBigNumber = BigInt(8515154151515151515151541445)
+console.log(aBigNumber);
+console.log(anotherBigNumber);
---
+++ src/es11/02-nullish.js
@@ -0,0 +1,3 @@
+const anotherNumber = 1;// o null en vez de  1
+const validete  = anotherNumber ?? 5;
+console.log(validete);
