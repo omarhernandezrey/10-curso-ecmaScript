5dadf5811dc8b98b5fe49e7c22839da27196f534
omarhernandezrey <hernandezreyomar@gmail.com>
2023-09-09 11:37:03 -0500

    30 ES11: dynamic Import

--- src/es11/06-dynamic-import.js
+++ src/es11/06-dynamic-import.js
@@ -1,6 +1,7 @@
-const button = document.getElementById('btn');
+const button = document.getElementById("btn");
 
-button.addEventListener('click', async function () {
-    const module = await import ('./module.js');
+button.addEventListener("click", async function () {
+    const module = await import("./module.js");
+    console.log(module);
     module.hello();
-});
+})
